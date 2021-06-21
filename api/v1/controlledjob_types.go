db0249b355e10272525e79bdde88af8641396478
Mark Pim <j.mark.pim@gmail.com>
2021-06-21 22:04:39 +0100

    Support friendly cron spec

--- api/v1/controlledjob_types.go
+++ api/v1/controlledjob_types.go
@@ -17,6 +17,10 @@ limitations under the License.
 package v1
 
 import (
+	"errors"
+	"fmt"
+	"regexp"
+
 	batchv1beta1 "k8s.io/api/batch/v1beta1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -65,15 +69,57 @@ type EventSpec struct {
 
 	// CronSchedule can contain an arbitrary Golang Cron Schedule
 	// (see https://pkg.go.dev/github.com/robfig/cron#hdr-CRON_Expression_Format)
-	// If set, schedule must NOT be set
+	// If set, takes precedence over Schedule
 	// +optional
 	CronSchedule string `json:"cronSchedule,omitempty"`
 
 	// Schedule is a more user friendly way to specify an event schedule
 	// It's more limited than the format supported by CronSchedule
-	// If set, cronSchedule must NOT be set
 	Schedule FriendlyScheduleSpec `json:"schedule,omitempty"`
 }
+
+var (
+	timeOfDayRegex  = regexp.MustCompile(`^(\d{2}):(\d{2})$`)
+	daysOfWeekRegex = regexp.MustCompile(`(?:^([a-zA-Z]{3})(?:,([a-zA-Z]{3}))?(?:,([a-zA-Z]{3}))?(?:,([a-zA-Z]{3}))?(?:,([a-zA-Z]{3}))?(?:,([a-zA-Z]{3}))?(?:,([a-zA-Z]{3}))?)$|^(?P<startRange>[a-zA-Z]{3})-(?P<endRange>[a-zA-Z]{3}$)`)
+)
+
+func (e *EventSpec) AsCronSpec() (string, error) {
+	if e.CronSchedule != "" {
+		return e.CronSchedule, nil
+	}
+	if e.Schedule.TimeOfDay == "" || e.Schedule.DaysOfWeek == "" {
+		return "", errors.New("Must specify either cronSchedule or schedule")
+	}
+	timeOfDayMatches := timeOfDayRegex.FindStringSubmatch(e.Schedule.TimeOfDay)
+	if len(timeOfDayMatches) != 3 {
+		return "", errors.New("daysOfWeek must be in the format MON-FRI or SAT,SUN,TUE,WED")
+	}
+
+	if !daysOfWeekRegex.Match([]byte(e.Schedule.DaysOfWeek)) {
+		return "", errors.New("timeOfDay must be in the format hh:mm")
+	}
+
+	return fmt.Sprintf("0 %s %s * * %s", timeOfDayMatches[2], timeOfDayMatches[1], e.Schedule.DaysOfWeek), nil
+}
+
+// ConcurrencyPolicy describes how the job will be handled.
+// Only one of the following concurrent policies may be specified.
+// If none of the following policies is specified, the default one
+// is AllowConcurrent.
+// +kubebuilder:validation:Enum=Allow;Forbid;Replace
+type ConcurrencyPolicy string
+
+const (
+	// AllowConcurrent allows CronJobs to run concurrently.
+	AllowConcurrent ConcurrencyPolicy = "Allow"
+
+	// ForbidConcurrent forbids concurrent runs, skipping next run if previous
+	// hasn't finished yet.
+	ForbidConcurrent ConcurrencyPolicy = "Forbid"
+
+	// ReplaceConcurrent cancels currently running job and replaces it with a new one.
+	ReplaceConcurrent ConcurrencyPolicy = "Replace"
+)
 
 // ControlledJobSpec defines the desired state of ControlledJob
 type ControlledJobSpec struct {
@@ -87,6 +133,40 @@ type ControlledJobSpec struct {
 
 	// Specifies the job that will be created when executing a CronJob.
 	JobTemplate batchv1beta1.JobTemplateSpec `json:"jobTemplate"`
+
+	//+kubebuilder:validation:Minimum=0
+
+	// Optional deadline in seconds for starting the job if it misses scheduled
+	// time for any reason.  Missed jobs executions will be counted as failed ones.
+	// +optional
+	StartingDeadlineSeconds *int64 `json:"startingDeadlineSeconds,omitempty"`
+
+	// Specifies how to treat concurrent executions of a Job.
+	// Valid values are:
+	// - "Allow" (default): allows CronJobs to run concurrently;
+	// - "Forbid": forbids concurrent runs, skipping next run if previous run hasn't finished yet;
+	// - "Replace": cancels currently running job and replaces it with a new one
+	// +optional
+	ConcurrencyPolicy ConcurrencyPolicy `json:"concurrencyPolicy,omitempty"`
+
+	// This flag tells the controller to suspend subsequent executions, it does
+	// not apply to already started executions.  Defaults to false.
+	// +optional
+	Suspend *bool `json:"suspend,omitempty"`
+
+	//+kubebuilder:validation:Minimum=0
+
+	// The number of successful finished jobs to retain.
+	// This is a pointer to distinguish between explicit zero and not specified.
+	// +optional
+	SuccessfulJobsHistoryLimit *int32 `json:"successfulJobsHistoryLimit,omitempty"`
+
+	//+kubebuilder:validation:Minimum=0
+
+	// The number of failed finished jobs to retain.
+	// This is a pointer to distinguish between explicit zero and not specified.
+	// +optional
+	FailedJobsHistoryLimit *int32 `json:"failedJobsHistoryLimit,omitempty"`
 }
 
 // ControlledJobStatus defines the observed state of ControlledJob
